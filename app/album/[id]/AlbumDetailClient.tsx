6e423d5486a9a9cb6604d7340f01310e3d80aae4
Chad <chad.farrow@gmail.com>
2025-07-22 12:18:59 -0400

    🔧 Fix album detail page to load all RSS feeds for better album discovery

--- app/album/[id]/AlbumDetailClient.tsx
+++ app/album/[id]/AlbumDetailClient.tsx
@@ -158,13 +158,84 @@ export default function AlbumDetailClient({ albumTitle, initialAlbum }: AlbumDet
           if (specificFeed) {
             feedUrls = [specificFeed];
           } else {
-            // Fallback to main feeds
+            // Load all feeds to find the album
             feedUrls = [
+              // Main Doerfels feeds
               'https://www.doerfelverse.com/feeds/music-from-the-doerfelverse.xml',
               'https://www.doerfelverse.com/feeds/bloodshot-lies-album.xml',
               'https://www.doerfelverse.com/feeds/intothedoerfelverse.xml',
               'https://www.doerfelverse.com/feeds/wrath-of-banjo.xml',
               'https://www.doerfelverse.com/feeds/ben-doerfel.xml',
+              
+              // Additional Doerfels albums and projects
+              'https://www.doerfelverse.com/feeds/18sundays.xml',
+              'https://www.doerfelverse.com/feeds/alandace.xml',
+              'https://www.doerfelverse.com/feeds/autumn.xml',
+              'https://www.doerfelverse.com/feeds/christ-exalted.xml',
+              'https://www.doerfelverse.com/feeds/come-back-to-me.xml',
+              'https://www.doerfelverse.com/feeds/dead-time-live-2016.xml',
+              'https://www.doerfelverse.com/feeds/dfbv1.xml',
+              'https://www.doerfelverse.com/feeds/dfbv2.xml',
+              'https://www.doerfelverse.com/feeds/disco-swag.xml',
+              'https://www.doerfelverse.com/feeds/doerfels-pubfeed.xml',
+              'https://www.doerfelverse.com/feeds/first-married-christmas.xml',
+              'https://www.doerfelverse.com/feeds/generation-gap.xml',
+              'https://www.doerfelverse.com/feeds/heartbreak.xml',
+              'https://www.doerfelverse.com/feeds/merry-christmix.xml',
+              'https://www.doerfelverse.com/feeds/middle-season-let-go.xml',
+              'https://www.doerfelverse.com/feeds/phatty-the-grasshopper.xml',
+              'https://www.doerfelverse.com/feeds/possible.xml',
+              'https://www.doerfelverse.com/feeds/pour-over.xml',
+              'https://www.doerfelverse.com/feeds/psalm-54.xml',
+              'https://www.doerfelverse.com/feeds/sensitive-guy.xml',
+              'https://www.doerfelverse.com/feeds/they-dont-know.xml',
+              'https://www.doerfelverse.com/feeds/think-ep.xml',
+              'https://www.doerfelverse.com/feeds/underwater-single.xml',
+              'https://www.doerfelverse.com/feeds/unsound-existence.xml',
+              'https://www.doerfelverse.com/feeds/you-are-my-world.xml',
+              'https://www.doerfelverse.com/feeds/you-feel-like-home.xml',
+              'https://www.doerfelverse.com/feeds/your-chance.xml',
+              
+              // Ed Doerfel (Shredward) projects
+              'https://www.sirtjthewrathful.com/wp-content/uploads/2023/08/Nostalgic.xml',
+              'https://www.sirtjthewrathful.com/wp-content/uploads/2023/08/CityBeach.xml',
+              'https://www.sirtjthewrathful.com/wp-content/uploads/2023/08/Kurtisdrums-V1.xml',
+              
+              // TJ Doerfel projects
+              'https://www.thisisjdog.com/media/ring-that-bell.xml',
+              
+              // Wavlake feeds - Nate Johnivan collection
+              'https://wavlake.com/feed/music/d677db67-0310-4813-970e-e65927c689f1',
+              'https://wavlake.com/feed/artist/aa909244-7555-4b52-ad88-7233860c6fb4',
+              'https://wavlake.com/feed/music/e678589b-5a9f-4918-9622-34119d2eed2c',
+              'https://wavlake.com/feed/music/3a152941-c914-43da-aeca-5d7c58892a7f',
+              'https://wavlake.com/feed/music/a97e0586-ecda-4b79-9c38-be9a9effe05a',
+              'https://wavlake.com/feed/music/0ed13237-aca9-446f-9a03-de1a2d9331a3',
+              'https://wavlake.com/feed/music/ce8c4910-51bf-4d5e-a0b3-338e58e5ee79',
+              'https://wavlake.com/feed/music/acb43f23-cfec-4cc1-a418-4087a5378129',
+              'https://wavlake.com/feed/music/d1a871a7-7e4c-4a91-b799-87dcbb6bc41d',
+              'https://wavlake.com/feed/music/3294d8b5-f9f6-4241-a298-f04df818390c',
+              'https://wavlake.com/feed/music/d3145292-bf71-415f-a841-7f5c9a9466e1',
+              'https://wavlake.com/feed/music/91367816-33e6-4b6e-8eb7-44b2832708fd',
+              'https://wavlake.com/feed/music/8c8f8133-7ef1-4b72-a641-4e1a6a44d626',
+              'https://wavlake.com/feed/music/9720d58b-22a5-4047-81de-f1940fec41c7',
+              'https://wavlake.com/feed/music/21536269-5192-49e7-a819-fab00f4a159e',
+              'https://wavlake.com/feed/music/624b19ac-5d8b-4fd6-8589-0eef7bcb9c9e',
+              
+              // Joe Martin (Wavlake) - Complete collection
+              'https://www.wavlake.com/feed/95ea253a-4058-402c-8503-204f6d3f1494',
+              'https://wavlake.com/feed/artist/18bcbf10-6701-4ffb-b255-bc057390d738',
+              'https://wavlake.com/feed/music/1c7917cc-357c-4eaf-ab54-1a7cda504976',
+              'https://wavlake.com/feed/music/e1f9dfcb-ee9b-4a6d-aee7-189043917fb5',
+              'https://wavlake.com/feed/music/d4f791c3-4d0c-4fbd-a543-c136ee78a9de',
+              'https://wavlake.com/feed/music/51606506-66f8-4394-b6c6-cc0c1b554375',
+              'https://wavlake.com/feed/music/6b7793b8-fd9d-432b-af1a-184cd41aaf9d',
+              'https://wavlake.com/feed/music/0bb8c9c7-1c55-4412-a517-572a98318921',
+              'https://wavlake.com/feed/music/16e46ed0-b392-4419-a937-a7815f6ca43b',
+              'https://wavlake.com/feed/music/2cd1b9ea-9ef3-4a54-aa25-55295689f442',
+              'https://wavlake.com/feed/music/33eeda7e-8591-4ff5-83f8-f36a879b0a09',
+              'https://wavlake.com/feed/music/32a79df8-ec3e-4a14-bfcb-7a074e1974b9',
+              'https://wavlake.com/feed/music/06376ab5-efca-459c-9801-49ceba5fdab1'
             ];
           }
           
